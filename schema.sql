38d214f8f5892fef65194734c373a54baa0a8b3b
Jabari Tucker <jabari_tucker@hotmail.com>
2021-07-26 16:53:17 -0600

    Created retirement info

--- schema.sql
+++ schema.sql
@@ -94,6 +94,26 @@ CREATE TABLE titles (
 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
 );
 
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
 SELECT * FROM salaries;
 
 SELECT * FROM titles;
